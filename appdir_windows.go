4cf92b2e4f5ad524e5cc62c3bb6f8f3dcacfac0b
Dorian Heinrichs <dorian.heinrichs@xiamli.com>
2024-12-13 22:31:30 +0100

    Removed windows implemention temporarily

--- appdir_windows.go
+++ appdir_windows.go
@@ -1,49 +1,41 @@
-package appdir
-
-import (
-	"os"
-	"path/filepath"
-	"sync"
-
-	"golang.org/x/sys/windows"
-)
+package appdirs
 
 type dirs struct {
 	name string
 }
 
-var initOnce sync.Once
-var localAppData string
-var roamingAppData string
-
-func initFolders() {
-	var err error
-	localAppData, err = windows.KnownFolderPath(windows.FOLDERID_LocalAppData, 0)
-	if err != nil {
-		localAppData = os.Getenv("LOCALAPPDATA")
-	}
-	roamingAppData, err = windows.KnownFolderPath(windows.FOLDERID_RoamingAppData, 0)
-	if err != nil {
-		roamingAppData = os.Getenv("APPDATA")
-	}
+func (d dirs) homeDir() string {
+	panic("Windows is not yet implemented")
 }
 
-func (d *dirs) UserConfig() string {
-	initOnce.Do(initFolders)
-	return filepath.Join(roamingAppData, d.name)
+func (d dirs) UserData() string {
+	panic("Windows is not yet implemented")
 }
 
-func (d *dirs) UserCache() string {
-	initOnce.Do(initFolders)
-	return filepath.Join(localAppData, d.name)
+func (d dirs) UserConfig() string {
+	panic("Windows is not yet implemented")
 }
 
-func (d *dirs) UserLogs() string {
-	initOnce.Do(initFolders)
-	return filepath.Join(localAppData, d.name)
+func (d dirs) UserCache() string {
+	panic("Windows is not yet implemented")
 }
 
-func (d *dirs) UserData() string {
-	initOnce.Do(initFolders)
-	return filepath.Join(localAppData, d.name)
+func (d dirs) UserLogs() string {
+	panic("Windows is not yet implemented")
+}
+
+func (d dirs) SharedData() string {
+	panic("Windows is not yet implemented")
+}
+
+func (d dirs) SharedConfig() string {
+	panic("Windows is not yet implemented")
+}
+
+func (d dirs) SharedCache() string {
+	panic("Windows is not yet implemented")
+}
+
+func (d dirs) SharedLogs() string {
+	panic("Windows is not yet implemented")
 }
